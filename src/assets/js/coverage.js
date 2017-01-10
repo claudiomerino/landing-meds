68c85bfb06cbd3ce4d6d7bb4808d69464fcf5ead
Jair <ejairn@gmail.com>
2017-01-09 22:43:58 -0500

    Rangos de edad

--- src/assets/js/coverage.js
+++ src/assets/js/coverage.js
@@ -1,23 +1,75 @@
 
-	let $activeCoverage = $( '.activeCoverage' );
-	let $activeCapital = $( '.activeCapital' );
-	let $activeDeductible = $( '.activeDeductible' );
+	// Tab 2
 
-	selectCoverageFn();
+	let $activeCoverageLesiones = $( '.activeCoverageLesiones' );
+	let $activeCapitalLesiones = $( '.activeCapitalLesiones' );
+	let $activeDeductibleLesiones = $( '.activeDeductibleLesiones' );
 
-	$activeCoverage.on( 'change', () => {
-		selectCoverageFn();
+	selectCoverageLesionesFn();
+
+	$activeCoverageLesiones.on( 'change', () => {
+		selectCoverageLesionesFn();
 	});
 
-	function selectCoverageFn() {
-		$.each( $activeCoverage, ( k, v ) => {
+	function selectCoverageLesionesFn() {
+		$.each( $activeCoverageLesiones, ( k, v ) => {
 
-			let $valCoverage = $( v ).val();
-			let $valCapital = $( $activeCapital ).data('c' + $valCoverage);
-			let $valDeductible = $( $activeDeductible ).data('c' + $valCoverage);
+			let $valCoverageLesiones = $( v ).val();
+			let $valCapitalLesiones = $( $activeCapitalLesiones ).data('c' + $valCoverageLesiones);
+			let $valDeductibleLesiones = $( $activeDeductibleLesiones ).data('c' + $valCoverageLesiones);
 
-			$activeCapital.html( $valCapital );
-			$activeDeductible.html( $valDeductible );
+			$activeCapitalLesiones.html( $valCapitalLesiones );
+			$activeDeductibleLesiones.html( $valDeductibleLesiones );
+
+		})
+	}
+
+	// Tab 3
+
+	let $activeCoverageComp = $( '.activeCoverageComp' );
+	let $activeCapitalComp = $( '.activeCapitalComp' );
+	let $activeDeductibleComp = $( '.activeDeductibleComp' );
+
+	selectCoverageCompFn();
+
+	$activeCoverageComp.on( 'change', () => {
+		selectCoverageCompFn();
+	});
+
+	function selectCoverageCompFn() {
+		$.each( $activeCoverageComp, ( k, v ) => {
+
+			let $valCoverageComp = $( v ).val();
+			let $valCapitalComp = $( $activeCapitalComp ).data('c' + $valCoverageComp);
+			let $valDeductibleComp = $( $activeDeductibleComp ).data('c' + $valCoverageComp);
+
+			$activeCapitalComp.html( $valCapitalComp );
+			$activeDeductibleComp.html( $valDeductibleComp );
+
+		})
+	}
+
+	// Tab 4
+
+	let $activeCoverageLast = $( '.activeCoverageLast' );
+	let $activeCapitalLast = $( '.activeCapitalLast' );
+	let $activeDeductibleLast = $( '.activeDeductibleLast' );
+
+	selectCoverageLastFn();
+
+	$activeCoverageLast.on( 'change', () => {
+		selectCoverageLastFn();
+	});
+
+	function selectCoverageLastFn() {
+		$.each( $activeCoverageLast, ( k, v ) => {
+
+			let $valCoverageLast = $( v ).val();
+			let $valCapital = $( $activeCapitalLast ).data('c' + $valCoverageLast);
+			let $valDeductible = $( $activeDeductibleLast ).data('c' + $valCoverageLast);
+
+			$activeCapitalLast.html( $valCapital );
+			$activeDeductibleLast.html( $valDeductible );
 
 		})
 	}
