d7e83992bda9224963a451a4c36373735b29550e
Jair <ejairn@gmail.com>
2017-01-13 16:11:35 -0500

    Menu principal

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -8,6 +8,10 @@
 	let $tabThird = $( '.tabThird' );
 	let $tabFourth = $( '.tabFourth' );
 	let $tabs = $( '#tabs' );
+	let $navTabFirst = $( '#navTabFirst' );
+	let $navTabSecond = $( '#navTabSecond' );
+	let $navTabThird = $( '#navTabThird' );
+	let $navTabFourth = $( '#navTabFourth' );
 
 	$headerMenu.on( 'click', () => {
 		$headerMenu.toggleClass( 'visible' );
@@ -31,34 +35,65 @@
 	  $headerList.removeClass( 'visible' );
 	})
 
-	$tabFirst.on( 'click', () => {
+	$tabFirst.add( $navTabFirst ).on( 'click', () => {
 		$tabs.foundation('selectTab', 'panel1');
+
 		$tabFirst.addClass( 'active-tab' );
+		$navTabFirst.addClass( 'active-nav-tab' );
+
+
 		$tabSecond.removeClass( 'active-tab' );
 		$tabThird.removeClass( 'active-tab' );
 		$tabFourth.removeClass( 'active-tab' );
+
+		$navTabSecond.removeClass( 'active-nav-tab' );
+		$navTabThird.removeClass( 'active-nav-tab' );
+		$navTabFourth.removeClass( 'active-nav-tab' );
 	})
 
-	$tabSecond.on( 'click', () => {
+	$tabSecond.add( $navTabSecond ).on( 'click', () => {
 		$tabs.foundation('selectTab', 'panel2');
+
 		$tabSecond.addClass( 'active-tab' );
+		$navTabSecond.addClass( 'active-nav-tab' );
+
+
 		$tabFirst.removeClass( 'active-tab' );
 		$tabThird.removeClass( 'active-tab' );
 		$tabFourth.removeClass( 'active-tab' );
+
+
+		$navTabFirst.removeClass( 'active-nav-tab' );
+		$navTabThird.removeClass( 'active-nav-tab' );
+		$navTabFourth.removeClass( 'active-nav-tab' );
 	})
 
-	$tabThird.on( 'click', () => {
+	$tabThird.add( $navTabThird ).on( 'click', () => {
 		$tabs.foundation('selectTab', 'panel3');
+
 		$tabThird.addClass( 'active-tab' );
+		$navTabThird.addClass( 'active-nav-tab' );
+
 		$tabFirst.removeClass( 'active-tab' );
 		$tabSecond.removeClass( 'active-tab' );
 		$tabFourth.removeClass( 'active-tab' );
+
+		$navTabFirst.removeClass( 'active-nav-tab' );
+		$navTabSecond.removeClass( 'active-nav-tab' );
+		$navTabFourth.removeClass( 'active-nav-tab' );
 	})
 
-	$tabFourth.on( 'click', () => {
+	$tabFourth.add( $navTabFourth ).on( 'click', () => {
 		$tabs.foundation('selectTab', 'panel4');
+
 		$tabFourth.addClass( 'active-tab' );
+		$navTabFourth.addClass( 'active-nav-tab' );
+
 		$tabFirst.removeClass( 'active-tab' );
 		$tabSecond.removeClass( 'active-tab' );
 		$tabThird.removeClass( 'active-tab' );
+
+		$navTabFirst.removeClass( 'active-nav-tab' );
+		$navTabSecond.removeClass( 'active-nav-tab' );
+		$navTabThird.removeClass( 'active-nav-tab' );
 	})
