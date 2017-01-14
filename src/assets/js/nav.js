f112c0533f66d812189d42fe693f86496abc32c6
Jair <ejairn@gmail.com>
2017-01-14 18:53:53 -0500

    Tabs con navegación

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -24,6 +24,10 @@
 		}
 	});
 
+	let hash = window.location.hash;
+
+	console.log( hash );
+
 
 	$headerContainer.on('sticky.zf.stuckto:top', function() {
 		$headerMenu.addClass( 'visible' );
@@ -97,3 +101,93 @@
 		$navTabSecond.removeClass( 'active-nav-tab' );
 		$navTabThird.removeClass( 'active-nav-tab' );
 	})
+
+
+
+	let $panel1 = $( '#panel1' );
+	let $panel2 = $( '#panel2' );
+	let $panel3 = $( '#panel3' );
+	let $panel4 = $( '#panel4' );
+
+
+
+	if( hash == '#panel1' ) {
+
+
+		$tabFirst.addClass( 'active-tab is-active' );
+		$navTabFirst.addClass( 'active-nav-tab' );
+
+		$panel1.addClass( 'is-active' );
+		$panel2.removeClass( 'is-active' );
+		$panel3.removeClass( 'is-active' );
+		$panel4.removeClass( 'is-active' );
+
+
+		$tabSecond.removeClass( 'active-tab is-active' );
+		$tabThird.removeClass( 'active-tab is-active' );
+		$tabFourth.removeClass( 'active-tab is-active' );
+
+		$navTabSecond.removeClass( 'active-nav-tab' );
+		$navTabThird.removeClass( 'active-nav-tab' );
+		$navTabFourth.removeClass( 'active-nav-tab' );
+	}
+	else if( hash == '#panel2' ) {
+
+
+		$panel1.removeClass( 'is-active' );
+		$panel2.addClass( 'is-active' );
+		$panel3.removeClass( 'is-active' );
+		$panel4.removeClass( 'is-active' );
+
+
+		$tabSecond.addClass( 'active-tab is-active' );
+		$navTabSecond.addClass( 'active-nav-tab' );
+
+
+		$tabFirst.removeClass( 'active-tab is-active' );
+		$tabThird.removeClass( 'active-tab is-active' );
+		$tabFourth.removeClass( 'active-tab is-active' );
+
+
+		$navTabFirst.removeClass( 'active-nav-tab' );
+		$navTabThird.removeClass( 'active-nav-tab' );
+		$navTabFourth.removeClass( 'active-nav-tab' );
+	}
+	else if( hash == '#panel3' ) {
+
+
+		$panel1.removeClass( 'is-active' );
+		$panel2.removeClass( 'is-active' );
+		$panel3.addClass( 'is-active' );
+		$panel4.removeClass( 'is-active' );
+
+		$tabThird.addClass( 'active-tab is-active' );
+		$navTabThird.addClass( 'active-nav-tab' );
+
+		$tabFirst.removeClass( 'active-tab is-active' );
+		$tabSecond.removeClass( 'active-tab is-active' );
+		$tabFourth.removeClass( 'active-tab is-active' );
+
+		$navTabFirst.removeClass( 'active-nav-tab' );
+		$navTabSecond.removeClass( 'active-nav-tab' );
+		$navTabFourth.removeClass( 'active-nav-tab' );
+	}
+	else if( hash == '#panel4' ) {
+
+
+		$panel1.removeClass( 'is-active' );
+		$panel2.removeClass( 'is-active' );
+		$panel3.removeClass( 'is-active' );
+		$panel4.addClass( 'is-active' );
+
+		$tabFourth.addClass( 'active-tab is-active' );
+		$navTabFourth.addClass( 'active-nav-tab' );
+
+		$tabFirst.removeClass( 'active-tab is-active' );
+		$tabSecond.removeClass( 'active-tab is-active' );
+		$tabThird.removeClass( 'active-tab is-active' );
+
+		$navTabFirst.removeClass( 'active-nav-tab' );
+		$navTabSecond.removeClass( 'active-nav-tab' );
+		$navTabThird.removeClass( 'active-nav-tab' );
+	}
