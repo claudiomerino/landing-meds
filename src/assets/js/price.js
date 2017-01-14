c1816d5f6e0ff2b3c31b094c1dd37c1c4ddde732
Jair <ejairn@gmail.com>
2017-01-14 17:26:52 -0500

    Total prima mensual

--- src/assets/js/price.js
+++ src/assets/js/price.js
@@ -1,6 +1,7 @@
 
 	let $selectSecure = $('select.secure');
 	let $selectSecureDate = $('select.secureDate');
+	let $selectSecureDateOption = $('select.secureDate option');
 	let $selectSecureDateCoverage = $('select.secureDateCoverage');
 	let $selectSecureDateCoverageOption = $('select.secureDateCoverage option');
 	let $selectSecureCan = $('select.secureCan');
@@ -12,137 +13,163 @@
 	let $pricePeso = $('.pricePeso');
 	let $priceUF = $('.priceUF');
 	let $priceSave = $('.priceSave');
-	let $changeCoverage = $('.changeCoverage')
-	let $changeCoverageMore = $('.changeCoverageMore')
+	let $changeCoverage = $('.changeCoverage');
+	let $changeCoverageMore = $('.changeCoverageMore');
 
-	const ufValuePeso = 26329.30;
+	const API = 'http://mindicador.cl/api/uf';
+	let ufValuePeso;
 
-	if( $selectSecure.val() == 'secureMayor' ) {
-		secureMayorFn();
-		$changeCoverage.removeClass( 'hide' );
-		$changeCoverageMore.addClass( 'hide' );
-	}
+	$.get( API )
+		.done( (res) => {
+			ufValuePeso = res.serie[0].valor;
 
-	$selectSecure.add( $selectSecureCan ).add( $selectSecureDate ).add( $selectSecureDateCoverage ).on( 'change', () => {
-		selectFn();
-	})
-
-	function selectFn() {
-		let $selectSecureVal = $selectSecure.val();
-
-		if( $selectSecureVal == 'secureMayor' ) {
-			secureMayorFn();
-			$changeCoverageMore.addClass( 'hide' );
-			$changeCoverage.removeClass( 'hide' );
-		}
-		else if( $selectSecureVal == 'secureMayorMore' ) {
-			secureMayorMoreFn();
-			$changeCoverage.addClass( 'hide' );
-			$changeCoverageMore.removeClass( 'hide' );
-		}
-
-		if( isNaN( $capital.html() ) || isNaN( $capital.html() ) ) {
-
-			$($selectSecureDateCoverage).val( '18' );
-
-			if( $selectSecureVal == 'secureMayor' ) {
+			if( $selectSecure.val() == 'secure1' ) {
+				$selectSecureDate.val('14');
 				secureMayorFn();
 			}
-			else if( $selectSecureVal == 'secureMayorMore' ) {
-				secureMayorMoreFn();
+
+			$selectSecureCan.add( $selectSecureDate ).add( $selectSecureDateCoverage ).on( 'change', () => {
+				selectFn();
+			})
+
+			$selectSecure.on( 'change', () => {
+				if( $selectSecure.val() == 'secure1' ) {
+					$selectSecureDate.val('14');
+				}
+				else if ( $selectSecure.val() == 'secure2' ) {
+					$selectSecureDate.val('214');
+				}
+				else if ( $selectSecure.val() == 'secure3' ) {
+					$selectSecureDate.val('314');
+				}
+				else if ( $selectSecure.val() == 'secure4' ) {
+					$selectSecureDate.val('414');
+				}
+
+				selectFn();
+			});
+
+			function selectFn() {
+				secureMayorFn();
 			}
 
-		}
+			function secureMayorFn() {
 
-	}
+				$.each( $selectSecureDate, ( k, v ) => {
 
-	function secureMayorFn() {
+					let $valueSecureDate = $( v ).val();
+					let $valueDataUf;
 
-		$.each( $selectSecureDate, ( k, v ) => {
+					if( $selectSecure.val() == 'secure1' ) {
+						$valueDataUf = $( $selectSecureCan ).find(':selected').data('uf' + $valueSecureDate);
+						$.each( $selectSecureDateOption, ( k, v ) => {
+							if( $(v).data('select') == 'secure1' ) {
+								$(v).show();
+							}
+							else {
+								$(v).hide();
+							}
+						})
+					}
 
-			let $valueSecureDate = $( v ).val();
-			let $valueDataUf = $( $selectSecureCan ).find(':selected').data('uf' + $valueSecureDate);
+					else if ( $selectSecure.val() == 'secure2' ) {
+						$valueDataUf = $( $selectSecureCan ).find(':selected').data('uf' + $valueSecureDate);
 
-			totalPrice( $valueDataUf );
+						$.each( $selectSecureDateOption, ( k, v ) => {
+							if( $(v).data('select') == 'secure2' ) {
+								$(v).show();
+							}
+							else {
+								$(v).hide();
+							}
+						})
+					}
+
+					else if ( $selectSecure.val() == 'secure3' ) {
+						$valueDataUf = $( $selectSecureCan ).find(':selected').data('uf' + $valueSecureDate);
+
+						$.each( $selectSecureDateOption, ( k, v ) => {
+							if( $(v).data('select') == 'secure3' ) {
+								$(v).show();
+							}
+							else {
+								$(v).hide();
+							}
+						})
+					}
+
+					else if ( $selectSecure.val() == 'secure4' ) {
+						$valueDataUf = $( $selectSecureCan ).find(':selected').data('uf' + $valueSecureDate);
+						$.each( $selectSecureDateOption, ( k, v ) => {
+							if( $(v).data('select') == 'secure4' ) {
+								$(v).show();
+							}
+							else {
+								$(v).hide();
+							}
+						})
+					}
+
+					totalPrice( $valueDataUf );
+
+				})
+
+				$.each( $selectSecureDateCoverage, ( k, v ) => {
+
+					let $valueSecureCvDate = $( v ).val();
+					let $valueDataCvUf = $( $capital ).data('cvuf' + $valueSecureCvDate);
+					let $valueDataDcUf = $( $deductible ).data('cvuf' + $valueSecureCvDate);
+					let $valueDataDcSickUf = $( $deductibleSick ).data('cvuf' + $valueSecureCvDate);
+
+					totalPriceCv( $valueDataCvUf );
+					totalPriceDc( $valueDataDcUf );
+					totalPriceDcSick( $valueDataDcSickUf );
+
+				})
+
+				let $priceMorePeso = $( $pricePeso ).html();
+				let unFormat = numeral().set($priceMorePeso).value();
+
+				$priceSave.html( parseFloat(unFormat * 3).toFixed(3) );
+
+			}
+
+			function totalPrice( UF ) {
+				if( ufValuePeso ) {
+					let totalPeso = UF * ufValuePeso;
+
+					$pricePeso.html( addInteger( totalPeso) );
+					$priceUF.html( addDecimal( UF ) );
+				}
+			}
+
+			function totalPriceCv( CvUf ) {
+				let totalPeso = CvUf;
+
+				$capital.html( addInteger( totalPeso ) );
+
+				if( totalPeso % 1 != 0 ) {
+					$capital.html( parseFloat( totalPeso ).toFixed(2) );
+				}
+
+			}
+
+			function totalPriceDc( CvUf ) {
+				let totalPeso = CvUf;
+				$deductible.html( addInteger( totalPeso ) );
+
+				if( totalPeso % 1 != 0 ) {
+					$deductible.html( totalPeso );
+				}
+
+			}
+
+			function totalPriceDcSick( Message ) {
+				$deductibleSick.html( Message );
+			}
+
 
 		})
-
-		$.each( $selectSecureDateCoverage, ( k, v ) => {
-
-			let $valueSecureCvDate = $( v ).val();
-			let $valueDataCvUf = $( $capital ).data('cvuf' + $valueSecureCvDate);
-			let $valueDataDcUf = $( $deductible ).data('cvuf' + $valueSecureCvDate);
-			let $valueDataDcSickUf = $( $deductibleSick ).data('cvuf' + $valueSecureCvDate);
-
-			totalPriceCv( $valueDataCvUf );
-			totalPriceDc( $valueDataDcUf );
-			totalPriceDcSick( $valueDataDcSickUf );
-
+		.fail( (err) => {
+			console.error( err );
 		})
-
-		let $priceMorePeso = $( $pricePeso ).html();
-		let unFormat = numeral().set($priceMorePeso).value();
-
-		$priceSave.html( parseFloat(unFormat * 3).toFixed(3) );
-
-	}
-
-	function secureMayorMoreFn() {
-		$.each( $selectSecureDate, ( k, v ) => {
-
-			let $valueSecureDate = $( v ).val();
-			let $valueDataUf = $( $selectSecureCan ).find(':selected').data('more-uf' + $valueSecureDate);
-
-			totalPrice( $valueDataUf );
-
-		})
-
-		$.each( $selectSecureDateCoverage, ( k, v ) => {
-
-			let $valueSecureCvDate = $( v ).val();
-			let $valueDataCvUf = $( $capital ).data('cvuf' + $valueSecureCvDate);
-			let $valueDataDcUf = $( $deductible ).data('cvuf' + $valueSecureCvDate);
-			let $valueDataDcSickUf = $( $deductibleSick ).data('cvuf' + $valueSecureCvDate);
-
-			totalPriceCv( $valueDataCvUf );
-			totalPriceDc( $valueDataDcUf );
-			totalPriceDcSick( $valueDataDcSickUf );
-
-		})
-
-		let $priceMorePeso = $( $pricePeso ).html();
-		let unFormat = numeral().set($priceMorePeso).value();
-
-		$priceSave.html( parseFloat(unFormat * 3).toFixed(3) );
-	}
-
-	function totalPrice( UF ) {
-		let totalPeso = UF * ufValuePeso;
-		$pricePeso.html( addInteger( totalPeso) );
-		$priceUF.html( addDecimal( UF ) );
-	}
-
-	function totalPriceCv( CvUf ) {
-		let totalPeso = CvUf;
-
-		$capital.html( addInteger( totalPeso ) );
-
-		if( totalPeso % 1 != 0 ) {
-			$capital.html( parseFloat( totalPeso ).toFixed(2) );
-		}
-
-	}
-
-	function totalPriceDc( CvUf ) {
-		let totalPeso = CvUf;
-		$deductible.html( addInteger( totalPeso ) );
-
-		if( totalPeso % 1 != 0 ) {
-			$deductible.html( totalPeso );
-		}
-
-	}
-
-	function totalPriceDcSick( Message ) {
-		$deductibleSick.html( Message );
-	}
